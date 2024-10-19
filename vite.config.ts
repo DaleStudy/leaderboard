a86ed565104a7c7fc27cbfedeed7bff53e39aa74
Dale Seo <dale.seo@gmail.com>
2024-10-19 18:46:05 -0400

    test: use happy-dom instead of jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: ["./src/vitest.setup.ts"],
   },
 });
