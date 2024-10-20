d1500361894eb6fb2391763c43efbf66d27bf3a9
sounmind <devleesche@gmail.com>
2024-10-19 23:35:50 -0400

    fix: resolve lint command error

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,20 @@
 import js from "@eslint/js";
-import tanstackPluginQuery from "@tanstack/eslint-plugin-query";
+import tanstackQueryPlugin from "@tanstack/eslint-plugin-query";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import eslintPluginStorybook from "eslint-plugin-storybook";
+import storybookPlugin from "eslint-plugin-storybook";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      eslintPluginStorybook,
-      tanstackPluginQuery,
-      eslintPluginPrettierRecommended,
-      eslintConfigPrettier,
-    ],
+    ignores: ["dist", "!.storybook", "styled-system", "postcss.config.cjs"],
+  },
+  {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -27,6 +23,13 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "@tanstack/query": tanstackQueryPlugin,
+
+      /**
+       * NOTE: Might not work properly with ESLINT version 9
+       * @see https://github.com/storybookjs/eslint-plugin-storybook/issues/157
+       */
+      storybook: storybookPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -36,4 +39,6 @@ export default tseslint.config(
       ],
     },
   },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 );
