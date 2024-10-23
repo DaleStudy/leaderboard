ac1537cd04a5b9c4e2751c51207d187c8afef3ae
SeonjaeLee <tjswo9502@gmail.com>
2024-10-23 16:36:29 +0900

    fix: convert CSF 3.0

--- src/components/Certificate/certificate.stories.tsx
+++ src/components/Certificate/certificate.stories.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import Certificate from "./certificate";
+import type { Meta, StoryObj } from "@storybook/react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+import Certificate from "./certificate";
 
 const username = "testUser";
 const pathname = `/members/${username}/certificate`;
 
-const Template = () => (
-  <MemoryRouter initialEntries={[pathname]}>
-    <Routes>
-      <Route path="/members/:username/certificate" element={<Certificate />} />
-    </Routes>
-  </MemoryRouter>
-);
+const meta: Meta<typeof Certificate> = {
+  component: Certificate,
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={[pathname]}>
+        <Routes>
+          <Route path="/members/:username/certificate" element={<Story />} />
+        </Routes>
+      </MemoryRouter>
+    ),
+  ],
+};
 
-export const Default = Template.bind({});
+export default meta;
 
-export default {};
+export const Default: StoryObj<typeof Certificate> = {};
