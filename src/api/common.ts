8ac972051c74ed6894c156954db1a2386b190977
HC-kang <weston0713@gmail.com>
2024-10-31 08:30:47 +0900

    remove: unused variables

--- src/api/common.ts
+++ src/api/common.ts
@@ -15,21 +15,3 @@ export function handleError(
 ): void {
   handler(message, error);
 }
-
-export const GITHUB_API_BASE_URL = "https://api.github.com";
-
-export const TOTAL_PROBLEMS = 75;
-
-export const ALTERNATIVE_IDS: Record<string, string> = {
-  // 1기
-  meoooh: "han",
-  koreas9408: "seunghyun-lim",
-  leokim0922: "leo",
-
-  // 2기
-  obzva: "flynn",
-  "kim-young": "kimyoung",
-  kjb512: "kayden",
-  lymchgmk: "egon",
-  jeonghwanmin: "hwanmini",
-};
