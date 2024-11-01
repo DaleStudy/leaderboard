f908618464306e06c38164a42727f95c977f401f
SeonjaeLee <tjswo9502@gmail.com>
2024-11-02 08:37:15 +0900

    story: Footer Component

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Footer from "./Footer";
+
+const meta = {
+  component: Footer,
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {};
