7bbd017164f5b0e69dacd47889bcad779c056091
HC-kang <weston0713@gmail.com>
2024-11-04 08:33:38 +0900

    remove: unused features

--- src/api/index.ts
+++
@@ -1,4 +0,0 @@
-import { CONFIG } from "./config";
-import { createStoreService } from "./services/store/storeService";
-
-export const leaderBoardStore = await createStoreService(CONFIG);
--- src/api/services/store/storeService.ts
+++ src/api/services/store/storeService.ts
@@ -1,49 +1,15 @@
 import type { Config } from "../../config/type";
-import type { MemberInfo } from "../common/types";
-import type { Grade } from "../../type";
 import { createFetchService } from "../fetch/fetchService";
 import { createProcessService } from "../process/processService";
 
-export async function createStoreService(config: Config) {
-  let fetchedData: MemberInfo[] = [];
+export async function fetchLeaderBoard(config: Config) {
   const fetchService = createFetchService(config);
   const processService = createProcessService(config);
 
-  const fetchAndProcessData = async (): Promise<MemberInfo[]> => {
-    const [members, submissions] = await Promise.all([
-      fetchService.fetchMembers(),
-      fetchService.fetchSubmissions(config.study.repository),
-    ]);
+  const [members, submissions] = await Promise.all([
+    fetchService.fetchMembers(),
+    fetchService.fetchSubmissions(config.study.repository),
+  ]);
 
-    fetchedData = processService.analyzeMemberInfo(members, submissions);
-
-    return fetchedData;
-  };
-
-  const getData = async () => {
-    if (fetchedData.length) return fetchedData;
-    return fetchAndProcessData();
-  };
-
-  return {
-    getData,
-
-    getMemberById: async (id: string): Promise<MemberInfo[]> => {
-      const data = await getData();
-      if (!data) throw new Error("Data not found");
-      return data.filter((member) => member.id.includes(id.toLowerCase()));
-    },
-
-    getMemberByCohort: async (cohort: number): Promise<MemberInfo[]> => {
-      const data = await getData();
-      if (!data) throw new Error("Data not found");
-      return data.filter((member) => member.cohort === cohort);
-    },
-
-    getMemberByGrade: async (grade: Grade): Promise<MemberInfo[]> => {
-      const data = await getData();
-      if (!data) throw new Error("Data not found");
-      return data.filter((member) => member.grade === grade);
-    },
-  };
+  return processService.analyzeMemberInfo(members, submissions);
 }
