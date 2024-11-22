3a497fc70a6392f6ed3c5667f8a8ac9a611ebf7f
SamTheKorean <bumsuyi@gmail.com>
2024-11-21 22:37:26 -0500

    feat : implement aside component

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import styles from "./Aside.module.css";
 
 interface AsideProps {
@@ -20,51 +20,49 @@ export default function Aside({
 }: AsideProps) {
   const progressPercent = Math.min((solvedTasks / totalTasks) * 100, 100);
 
-  // Apply a CSS class based on progress percent
   useEffect(() => {
     document.documentElement.style.setProperty(
       "--progress",
-      `${progressPercent}%`
+      `${progressPercent}%`,
     );
   }, [progressPercent]);
 
   return (
-    <aside className={styles.container}>
-      <section className={styles.profile}>
-        <div className={styles.avatar}>
-          <div className={styles["progress-circle"]}></div>
-          <img src="path_to_avatar_image" alt="User's profile picture" />
-        </div>
-        <div className={styles.progress}>
-          <span>{solvedTasks} 문제</span>
-          <div>PR 리스트</div>
-        </div>
-        <p className={styles.username}>{githubUsername}</p>
-      </section>
+    <aside>
+      <div className={styles.container}>
+        <section className={styles.profile}>
+          <div className={styles.avatar}>
+            <div className={styles["progress-circle"]}></div>
+            <img src="/SampleImg.png" alt="User's profile picture" />
+          </div>
+          <div className={styles.progress}>
+            <span>{solvedTasks} 문제</span>
+            <div>PR 리스트</div>
+          </div>
+          <p className={styles.username}>{githubUsername}</p>
+        </section>
 
-      <section className={styles.currentStatus}>
-        <figure>
-          <img src="path_to_current_status_image" alt="현재 상태 아이콘" />
-          <figcaption>새싹</figcaption>
-        </figure>
-      </section>
-
-      <section className={styles.taskCounts}>
-        <div className={styles.task}>
-          <span className={styles.easy}>EASY</span>
-          <span>{easyTasks}</span>
-        </div>
-        <div className={styles.task}>
-          <span className={styles.medium}>MEDIUM</span>
-          <span>{mediumTasks}</span>
-        </div>
-        <div className={styles.task}>
-          <span className={styles.hard}>HARD</span>
-          <span>{hardTasks}</span>
-        </div>
-      </section>
-
-      <button className={styles.returnButton}>리스트로 돌아가기</button>
+        <section className={styles.currentStatus}>
+          <figure>
+            <img src="image_url" alt="현재 등급 아이콘" />
+          </figure>
+        </section>
+        <section className={styles.taskCounts}>
+          <div className={styles.task}>
+            <span className={styles.easy}>EASY</span>
+            <span>{easyTasks}</span>
+          </div>
+          <div className={styles.task}>
+            <span className={styles.medium}>MEDIUM</span>
+            <span>{mediumTasks}</span>
+          </div>
+          <div className={styles.task}>
+            <span className={styles.hard}>HARD</span>
+            <span>{hardTasks}</span>
+          </div>
+        </section>
+      </div>
+      <button className={styles.returnButton}>리더보드로 돌아가기</button>
     </aside>
   );
 }
