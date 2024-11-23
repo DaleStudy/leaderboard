2aa2da4a30793676734d9bc442d138cb51136910
Dale Seo <dale.seo@gmail.com>
2024-11-22 20:07:11 -0500

    refactor: improve table story

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -12,9 +12,27 @@ export const Default: StoryObj<typeof Table> = {
     problems: [
       {
         id: 1,
-        title: "Example Problem 1",
+        title: "Two Sum",
         difficulty: "Easy",
         completed: true,
+      },
+      {
+        id: 128,
+        title: "Longest Consecutive Sequence",
+        difficulty: "Med.",
+        completed: false,
+      },
+      {
+        id: 133,
+        title: "Clone Graph",
+        difficulty: "Med.",
+        completed: true,
+      },
+      {
+        id: 295,
+        title: "Find Median From Data Stream",
+        difficulty: "Hard",
+        completed: false,
       },
     ],
   },
