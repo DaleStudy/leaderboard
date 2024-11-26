35d436d766d48f50f2755c2e8580cf4cc5db15b2
sounmind <devleesche@gmail.com>
2024-11-25 22:48:46 -0500

    feat: apply updated useMember, SearchBar to Leaderboard

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,5 +1,3 @@
-import { useCallback, useState } from "react";
-
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import SearchBar from "../SearchBar/SearchBar";
@@ -11,28 +9,16 @@ import Card from "../Card/Card";
 import styles from "./Leaderboard.module.css";
 
 export default function Leaderboard() {
-  const { members, isLoading, error } = useMembers({ getMembers });
-  const [criteria, setCriteria] = useState<{
-    name: string;
-    cohort: number | null;
-  }>({ name: "", cohort: null });
+  const { members, isLoading, error, totalCohorts, filter, setFilter } =
+    useMembers({ getMembers });
 
-  const totalCohorts = new Set(members.map((member) => member.cohort)).size;
-  const handleSearch = useCallback(
-    (name: string, cohort: number | null): void =>
-      setCriteria({ name, cohort }),
-    [],
-  );
+  const handleSearch = (name: string, cohort: number | null): void =>
+    setFilter({ name, cohort });
 
   if (isLoading) return <p>Loading...</p>; // TODO replace with a proper loading component
   if (error) return <p>Error!</p>; // TODO replace with a proper error component
 
-  const processedMembers = members
-    .filter((member) => member.name.includes(criteria.name))
-    .filter(
-      (member) => criteria.cohort === null || member.cohort === criteria.cohort,
-    )
-    .sort((memberA, memberB) => memberB.progress - memberA.progress);
+  const sortedMembers = members.sort((a, b) => b.progress - a.progress);
 
   return (
     <main className={styles.leaderboard}>
@@ -40,11 +26,15 @@ export default function Leaderboard() {
 
       <section className={styles.toolbar}>
         <h1>리더보드</h1>
-        <SearchBar onSearch={handleSearch} totalCohorts={totalCohorts} />
+        <SearchBar
+          filter={filter}
+          onSearch={handleSearch}
+          totalCohorts={totalCohorts}
+        />
       </section>
 
       <ul>
-        {processedMembers.map((member) => (
+        {sortedMembers.map((member) => (
           <li key={member.id}>
             <Card
               id={member.id}
