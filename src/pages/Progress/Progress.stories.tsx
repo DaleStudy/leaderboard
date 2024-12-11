aec5aaba376a7f2b9a02a02f60ad29b15fccd459
SeonjaeLee <tjswo9502@gmail.com>
2024-12-11 23:15:47 +0900

    feature: Progress spinner 적용

--- src/pages/Progress/Progress.stories.tsx
+++ src/pages/Progress/Progress.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { http, HttpResponse } from "msw";
+import { delay, http, HttpResponse } from "msw";
+
 import Progress from "./Progress";
 
 const meta: Meta<typeof Progress> = {
@@ -33,7 +34,34 @@ export default meta;
 
 export const Default: StoryObj<typeof Progress> = {};
 
+export const Loading: StoryObj<typeof meta> = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://api.github.com/orgs/DaleStudy/teams", async () => {
+          await delay("infinite");
+        }),
+      ],
+    },
+  },
+};
 export const ServerError: StoryObj<typeof meta> = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://api.github.com/orgs/DaleStudy/teams", () =>
+          HttpResponse.error(),
+        ),
+        http.get(
+          "https://api.github.com/orgs/DaleStudy/teams/leetcode02/members",
+          () => HttpResponse.error(),
+        ),
+      ],
+    },
+  },
+};
+
+export const NotFound: StoryObj<typeof meta> = {
   parameters: {
     msw: {
       handlers: [
