d6cfeb9dcb15b25d2fdc0fc503f3bab9f6f83148
Dale Seo <dale.seo@gmail.com>
2025-01-25 15:16:36 -0500

    feat: disable certificate link for unqualified members

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -10,17 +10,27 @@ interface LinkProps
   extends HTMLAttributes<HTMLAnchorElement>,
     AnchorHTMLAttributes<HTMLAnchorElement> {
   variant?: "text" | "primaryButton" | "secondaryButton";
+  disabled?: boolean;
 }
 
 export default function Link({
   variant = "text",
   className,
   children,
+  href,
+  disabled = false,
   ...props
 }: PropsWithChildren<LinkProps>) {
+  if (disabled && variant !== "primaryButton") {
+    throw new Error(
+      "The `disabled` prop is only supported for the primaryButton variant",
+    );
+  }
+
   return (
     <a
       {...props}
+      href={disabled ? undefined : href}
       className={`${styles.default} ${styles[variant]} ${className ?? ""}`}
     >
       {children}
