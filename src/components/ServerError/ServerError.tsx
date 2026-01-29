bca02337d65f61d9cac889ca4a36c6a4d068bd3e
hyoseong1994 <wjdgytjd3291@gmail.com>
2026-01-29 20:52:30 +0900

    feat: apply daleui by server error

--- src/components/ServerError/ServerError.tsx
+++ src/components/ServerError/ServerError.tsx
@@ -1,26 +1,26 @@
-import Button from "../Button/Button";
+import { Button, Heading, Link, Text, VStack } from "daleui";
 import Meta from "../Meta/Meta";
-
-import styles from "./ServerError.module.css";
 
 export default function ServerError() {
   return (
-    <section className={styles.serverError}>
+    <VStack gap="36">
       <Meta title="서버 오류" description="서버 오류가 발생했습니다." />
 
-      <h2>Error</h2>
-      <p>
+      <Heading level={2} size={1} tone="neutral">
+        Error
+      </Heading>
+      <Text>
         오류가 발생했습니다. 문제가 지속된다면 아래 Github Issue를 방문하여{" "}
         <br />
         문제를 보고하거나 진행 상황을 확인해 주시면 감사하겠습니다.
-      </p>
-      <a
+      </Text>
+      <Link
         href="https://github.com/DaleStudy/leaderboard/issues"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Button variant="secondary">문제 보고하기</Button>
-      </a>
-    </section>
+        <Button variant="solid">문제 보고하기</Button>
+      </Link>
+    </VStack>
   );
 }
