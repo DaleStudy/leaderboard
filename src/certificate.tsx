53c925ab3e3309b0ef5b8b26478f19a3b7d78319
RiaOh <ken5840714@gmail.com>
2026-01-31 02:28:21 +0900

    feat: import daleui style in certificate

--- src/certificate.tsx
+++ src/certificate.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Certificate from "./pages/Certificate/Certificate";
 import "./index.css";
+import "daleui/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
